ca848fa4ce99cc15b87009aefd6d1fe85c1e8062
Mads Kristensen <madsarke@gmail.com>
2020-11-11 09:26:17 +0100

    dto

--- backend/src/main/java/DTOs/CombinedDTO.java
+++ backend/src/main/java/DTOs/CombinedDTO.java
@@ -10,5 +10,24 @@ package DTOs;
  * @author Mads
  */
 public class CombinedDTO {
+    String joke1;
+    String joke1Reference;
+    String joke2;
+    String joke2Reference;
+    String joke3;
+    String joke3Reference;
+    String joke4;
+    String joke4Reference;
+    String joke5;
+    String joke5Reference;
+
+    public CombinedDTO(String joke1, String joke2, String joke3, String joke4, String joke5) {
+        this.joke1 = joke1;
+        this.joke2 = joke2;
+        this.joke3 = joke3;
+        this.joke4 = joke4;
+        this.joke5 = joke5;
+    }
+    
     
 }
